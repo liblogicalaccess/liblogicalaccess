92f781705cefc677931b2fca40465782921edc6e
Adrien JUND <soultaikerdidu@gmail.com>
2013-11-25 17:09:35 +0100

    Fix sign script postbuild for auto sign all dll and exe

--- scripts/postbuild.bat
+++ scripts/postbuild.bat
@@ -1,6 +1,10 @@
 IF "%1" == "true" (
-	signtool sign /v /f %ISLOGRepository%keys\islog_sign_binary.pfx /p %ISLOGSIGN% /t %ISLOGSignTimestampSrv% bin/Win32/Release/*.dll
-	signtool sign /v /f %ISLOGRepository%keys\islog_sign_binary.pfx /p %ISLOGSIGN% /t %ISLOGSignTimestampSrv% bin/x64/Release/*.dll
-	signtool sign /v /f %ISLOGRepository%keys\islog_sign_binary.pfx /p %ISLOGSIGN% /t %ISLOGSignTimestampSrv% bin/Win32/Release/*.exe
-	signtool sign /v /f %ISLOGRepository%keys\islog_sign_binary.pfx /p %ISLOGSIGN% /t %ISLOGSignTimestampSrv% bin/x64/Release/*.exe
+	for /r bin %%d in (*.dll *.exe) do (
+	:while1
+	signtool sign /v /f %ISLOGRepository%keys\islog_sign_binary.pfx /p %ISLOGSIGN% /t %ISLOGSignTimestampSrv% %%d
+		@if %ERRORLEVEL% GEQ 1 (
+			sleep 1
+			goto :while1
+		)
+	)
 )
