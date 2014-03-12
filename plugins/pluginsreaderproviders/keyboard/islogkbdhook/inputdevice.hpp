782bb4653b08268d5f7da8849f9e9badd104bd3e
Adrien JUND <adrien.jund@epitech.eu>
2014-03-12 10:03:31 +0100

    rename c++ header to hpp

---
+++ plugins/pluginsreaderproviders/keyboard/islogkbdhook/inputdevice.hpp
@@ -0,0 +1,19 @@
+#ifndef _DEFINED_INPUTDEVICE
+#define _DEFINED_INPUTDEVICE
+
+#include "stdafx.h"
+#include <vector>
+
+#include "../keyboardsharedstruct.hpp"
+
+namespace KBDHOOK
+{
+	class InputDevice
+	{
+		public:
+
+			static std::vector<logicalaccess::KeyboardEntry> getDeviceList();
+	};
+}
+
+#endif
---
+++ plugins/pluginsreaderproviders/keyboard/keyboardsharedstruct.hpp
@@ -0,0 +1,41 @@
+/**
+ * \file keyboardsharedstruct.hpp
+ * \author Maxime C. <maxime-dev@islog.com>
+ * \brief Keyboard shared struct. 
+ */
+
+#ifndef LOGICALACCESS_KEYBOARDSHAREDSTRUCT_HPP
+#define LOGICALACCESS_KEYBOARDSHAREDSTRUCT_HPP
+
+namespace logicalaccess
+{
+	#define KEYBOARD_SHAREDDATA			"Global\\ISLOGKbdHook"
+	#define KEYBOARD_EVENT				"Global\\ISLOGKbdEvent"
+	#define KEYBOARD_EVENTPROCESEED		"Global\\ISLOGKbdEventProcessed"
+	#define KEYBOARD_HOSTEVENT			"Global\\ISLOGHostEvent"
+	#define KEYBOARD_STILLALIVEEVENT	"Global\\ISLOGStillAliveEvent"
+
+	#define MAX_KEYBOARD_DEVICES		10
+	#define DEVICE_NAME_MAXLENGTH		128
+
+	struct KeyboardEntry
+	{
+		unsigned long long handle;
+
+		char name[DEVICE_NAME_MAXLENGTH];
+	};
+
+	struct KeyboardSharedStruct
+	{
+		KeyboardEntry devices[MAX_KEYBOARD_DEVICES];
+
+		char enteredKeyChar;
+
+		char selectedDeviceName[DEVICE_NAME_MAXLENGTH];
+
+		char keyboardLayout[9];
+	};
+}
+
+#endif
+
