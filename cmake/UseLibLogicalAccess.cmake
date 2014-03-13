b0ac51e160d2fc3f854cd57bebc4157db2dc1153
jschueller <julien.schueller@gmail.com>
2014-03-13 09:38:55 +0100

    Added cmake config file

---
+++ cmake/UseLibLogicalAccess.cmake
@@ -0,0 +1,8 @@
+#                                               -*- cmake -*-
+#
+#  UseLibLogicalAccess.cmake
+#
+
+add_definitions     ( ${LIBLOGICALACCESS_DEFINITIONS} )
+include_directories ( ${LIBLOGICALACCESS_INCLUDE_DIRS} )
+link_directories    ( ${LIBLOGICALACCESS_LIBRARY_DIRS} )
