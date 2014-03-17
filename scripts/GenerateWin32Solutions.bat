47f6f9a8db61149262031e04dcb5b89449709aac
Arnaud Humilier <arnaud.humilier@islog.com>
2014-03-17 14:27:41 +0100

    Update Windows batch scripts

--- scripts/GenerateWin32Solutions.bat
+++ scripts/GenerateWin32Solutions.bat
@@ -1,9 +1,4 @@
 cd ..
 call scripts/cleancmake.bat
 cmake -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=Win32 -G"Visual Studio 11" -Tv110_xp .
-cd plugins/pluginscards/
-cmake -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=Win32 -G"Visual Studio 11" -Tv110_xp .
-cd ../pluginsreaderproviders/
-cmake -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=Win32 -G"Visual Studio 11" -Tv110_xp .
-cd ../../scripts
 pause;
