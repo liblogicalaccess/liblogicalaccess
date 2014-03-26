df7e62bb104be36378f80c303576aa384a9bf377
Adrien JUND <soultaikerdidu@gmail.com>
2014-03-26 14:11:25 +0100

    Fix basic script

--- scripts/GenerateBasicSolution.bat
+++
@@ -1,5 +0,0 @@
-call cleancmake.bat basic
-cd ../samples/basic
-cmake -G"Visual Studio 11" -Tv110_xp -DLibLogicalAccess_DIR=./../../install/lib/cmake/liblogicalaccess .
-cd ../..
-pause;
---
+++ scripts/GenerateX64BasicSolution.bat
@@ -0,0 +1,5 @@
+call cleancmake.bat basic
+cd ../samples/basic
+cmake -G"Visual Studio 11 Win64" -Tv110_xp -DLibLogicalAccess_DIR=./../../install/lib/cmake/liblogicalaccess -DCMAKE_INSTALL_PREFIX=../../install .
+cd ../..
+pause;
