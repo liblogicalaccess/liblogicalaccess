00fc6d62d2384d0439fa8604f8fd3969a0f957e2
Maxime Chamley <maxime.chamley@islog.com>
2014-04-09 16:44:43 +0200

    Change cmake install directories

--- scripts/GenerateX64Solutions-debug.bat
+++ scripts/GenerateX64Solutions-debug.bat
@@ -1,2 +1,2 @@
 call scripts\cleancmake.bat
-cmake.exe -DCMAKE_BUILD_TYPE=Debug -DTARGET_ARCH=x64 -G"Visual Studio 11 Win64" -Tv110_xp -DCMAKE_INSTALL_PREFIX=./install .
+cmake.exe -DCMAKE_BUILD_TYPE=Debug -DTARGET_ARCH=x64 -DLIB_SUFFIX=Debug -G"Visual Studio 11 Win64" -Tv110_xp -DCMAKE_INSTALL_PREFIX=./install .
--- scripts/postbuild.bat
+++ scripts/postbuild.bat
@@ -1,5 +1,5 @@
 IF "%1" == "true" (
-	for /r bin %%d in (*.dll *.exe) do (
+	for /r %2 %%d in (*.dll *.exe) do (
 	:while1
 	signtool sign /v /f %ISLOGRepository%keys\islog_sign_binary.pfx /p %ISLOGSIGN% /t %ISLOGSignTimestampSrv% %%d
 		@if %ERRORLEVEL% GEQ 1 (
