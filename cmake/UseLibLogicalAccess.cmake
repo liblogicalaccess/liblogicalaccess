c35c25152ee8262610cec7eadf065d8cdab771dc
Adrien JUND <adrien.jund@epitech.eu>
2014-04-09 17:44:46 +0200

    include logicalacces folder

--- cmake/UseLibLogicalAccess.cmake
+++ cmake/UseLibLogicalAccess.cmake
@@ -5,4 +5,5 @@
 
 add_definitions     ( ${LIBLOGICALACCESS_DEFINITIONS} )
 include_directories ( ${LIBLOGICALACCESS_INCLUDE_DIRS} )
+include_directories ( ${LIBLOGICALACCESS_INCLUDE_DIR}/.. )
 link_directories    ( ${LIBLOGICALACCESS_LIBRARY_DIRS} )
