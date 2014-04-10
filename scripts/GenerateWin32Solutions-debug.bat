e07bc882d6de1a4fb117af7b8e115c87e603dcae
Adrien JUND <soultaikerdidu@gmail.com>
2014-04-10 09:31:13 +0200

    Final Fix and clean CMake output file

--- scripts/GenerateWin32Solutions-debug.bat
+++ scripts/GenerateWin32Solutions-debug.bat
@@ -1,2 +1,2 @@
 call scripts\cleancmake.bat
-cmake.exe -DCMAKE_BUILD_TYPE=Debug -DTARGET_ARCH=Win32 -DLIB_SUFFIX=Debug -G"Visual Studio 11" -Tv110_xp . -DCMAKE_INSTALL_PREFIX=./install .
+cmake.exe -DCMAKE_BUILD_TYPE=Debug -DTARGET_ARCH=Win32 -DLIB_SUFFIX=Win32/Debug -G"Visual Studio 11" -Tv110_xp . -DCMAKE_INSTALL_PREFIX=./install .
--- scripts/GenerateX64Solutions.bat
+++ scripts/GenerateX64Solutions.bat
@@ -1,2 +1,2 @@
 call scripts\cleancmake.bat
-cmake.exe -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=x64 -DLIB_SUFFIX=Release -G"Visual Studio 11 Win64" -Tv110_xp . -DCMAKE_INSTALL_PREFIX=./install .
+cmake.exe -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=x64 -DLIB_SUFFIX=x64/Release -G"Visual Studio 11 Win64" -Tv110_xp . -DCMAKE_INSTALL_PREFIX=./install .
