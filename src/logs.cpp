fec4521baaf596f5b873bc2dac3128600d7a3087
Adrien JUND <soultaikerdidu@gmail.com>
2014-05-23 14:57:47 +0200

    Add missing logger file

---
+++ src/logs.cpp
@@ -0,0 +1,52 @@
+/**
+ * \file logs.cpp
+ * \author Maxime C. <maxime-dev@islog.com>
+ * \brief Base class logs informations. 
+ */
+ 
+#include "logicalaccess/logs.hpp"
+
+namespace logicalaccess
+{
+	std::ofstream Logs::logfile;
+	std::map<LogLevel, std::string> Logs::logLevelMsg;
+
+	Logs::Logs(const char* file, const char* func, int line, enum LogLevel level)
+	{
+		if (!Settings::getInstance()->IsLogEnabled
+			|| (level == LogLevel::COMS && !Settings::getInstance()->SeeCommunicationLog)
+			|| ((level == LogLevel::PLUGINS || level == LogLevel::PLUGINS_ERROR) && !Settings::getInstance()->SeePluginLog))
+			d_level = NONE;
+
+		if (logLevelMsg.empty())
+		{
+			logLevelMsg[INFOS] = "INFO";
+			logLevelMsg[WARNINGS] = "WARNING";
+			logLevelMsg[NOTICES] = "NOTICE";
+			logLevelMsg[ERRORS] = "ERROR";
+			logLevelMsg[EMERGENSYS] = "EMERGENSY";
+			logLevelMsg[CRITICALS] = "CRITICAL";
+			logLevelMsg[ALERTS] = "ALERT";
+			logLevelMsg[DEBUGS] = "DEBUG";
+			logLevelMsg[COMS] = "COM";
+			logLevelMsg[PLUGINS] = "PLUGIN";
+		}
+
+		if (logfile && d_level != NONE)
+		{
+			boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
+			_stream << boost::posix_time::to_simple_string(now) << " - " << logLevelMsg[level] << ": \t{" << line << "}\t{" << func << "}\t{" << file << "}:" << std::endl;
+		}
+	}
+
+	Logs::~Logs()
+	{
+		if (logfile && d_level != NONE)
+		{
+			_stream << std::endl;
+			logfile << _stream.rdbuf();
+			logfile.flush();
+		}
+	}
+}
+
