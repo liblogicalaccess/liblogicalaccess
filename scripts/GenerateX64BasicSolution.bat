7a4ccc804b9874ecb5d7355ab0a120acb894f212
Adrien JUND <soultaikerdidu@gmail.com>
2014-12-05 16:03:31 +0100

    Move to visual studio 2013 with boost 1.57

--- scripts/GenerateX64BasicSolution.bat
+++ scripts/GenerateX64BasicSolution.bat
@@ -1,4 +1,4 @@
 call scripts\cleancmake.bat basic
 cd samples\basic
-cmake.exe -G"Visual Studio 11 Win64" -Tv110_xp -DLibLogicalAccess_DIR=./../../install/lib/x64/Debug/cmake/liblogicalaccess -DCMAKE_INSTALL_PREFIX=../../install .
+cmake.exe -G"Visual Studio 12 Win64" -Tv120_xp -DLibLogicalAccess_DIR=./../../install/lib/x64/Debug/cmake/liblogicalaccess -DCMAKE_INSTALL_PREFIX=../../install .
 cd ..\..
--- scripts/GenerateX64Solutions-debug.bat
+++ scripts/GenerateX64Solutions-debug.bat
@@ -1,2 +1,2 @@
 call scripts\cleancmake.bat
-cmake.exe -DCMAKE_BUILD_TYPE=Debug -DTARGET_ARCH=x64 -DLIB_SUFFIX=x64/Debug -G"Visual Studio 11 Win64" -Tv110_xp -DCMAKE_INSTALL_PREFIX=./install .
+cmake.exe -DCMAKE_BUILD_TYPE=Debug -DTARGET_ARCH=x64 -DLIB_SUFFIX=x64/Debug -G"Visual Studio 12 Win64" -Tv120_xp -DCMAKE_INSTALL_PREFIX=./install .
