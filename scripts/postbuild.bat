e4f3ec9a9f36047ba95363c45e886a66d3e0fb4b
Maxhy <maxime.chamley@islog.com>
2016-02-17 09:30:38 +0100

    Update postbuild script to use fsign

--- scripts/postbuild.bat
+++ scripts/postbuild.bat
@@ -1,7 +1,7 @@
 IF "%1" == "true" (
 	for /r %2 %%d in (*.dll *.exe) do (
 	:while1
-	signtool sign /v /f %ISLOGRepository%keys\islog_sign_binary.pfx /p %ISLOGSIGN% /t %ISLOGSignTimestampSrv% "%%d"
+	fsign "%%d"
 		@if %ERRORLEVEL% GEQ 1 (
 			sleep 1
 			goto :while1
