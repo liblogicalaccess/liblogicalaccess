2f03b2b196c91307e8eb4ba7aa47532bda51d137
Adrien JUND <adrien.jund@islog.com>
2016-04-06 15:22:01 +0200

    Improve sign script

--- scripts/sign.ps1
+++ scripts/sign.ps1
@@ -1,7 +1,19 @@
 $files = Get-ChildItem -path $args[0] -recurse -Include *.exe,*.dll
 
-signtool sign /v /f $env:ISLOGRepository\keys\islog_sign_binary.pfx /p $env:ISLOGSIGN /t http://timestamp.verisign.com/scripts/timstamp.dll $files
-signtool sign /as /fd SHA256 /f $env:ISLOGRepository\keys\islog_sign_binary.pfx /p $env:ISLOGSIGN /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 $files
+while ($true)
+{
+	& signtool sign /v /f $env:ISLOGRepository\keys\islog_sign_binary.pfx /p $env:ISLOGSIGN /t http://timestamp.verisign.com/scripts/timstamp.dll $files
+	if ($LASTEXITCODE -eq 0) {
+		break;
+	}
+}
+while ($true)
+{
+	& signtool sign /as /fd SHA256 /f $env:ISLOGRepository\keys\islog_sign_binary.pfx /p $env:ISLOGSIGN /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 $files
+	if ($LASTEXITCODE -eq 0) {
+		break;
+	}
+}
 
 
 
