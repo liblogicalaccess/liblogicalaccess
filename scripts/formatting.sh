1beaae8f402edb2aa15a759f9057058c028827b1
Adrien JUND <adrien.jund@islog.com>
2017-10-18 13:53:03 +0200

    Add clang format

--- scripts/formatting.sh
+++ scripts/formatting.sh
@@ -4,40 +4,4 @@
 # When a file goes through many modification, it may be worth to profit and
 # fix its formatting.
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-clang-format-3.8 -style=file -i ${DIR}/../include/logicalaccess/cards/IKSStorage.hpp
-find ${DIR}/../include/logicalaccess/iks -name "*.hpp"  | xargs clang-format-3.8 -style=file -i {} \;
-
-find ${DIR}/../src/iks -name "*.cpp"  | xargs clang-format-3.8 -style=file -i {} \;
-
-
-find ${DIR}/../tests/other/ -name "*.cpp"  | xargs clang-format-3.8 -style=file -i {} \;
-clang-format-3.8 -style=file -i ${DIR}/../tests/pcsc-tests/DESFireEV1/test_changekey_iks.cpp
-
-clang-format-3.8 -style=file -i ${DIR}/../src/cards/keystorage.cpp
-
-clang-format-3.8 -style=file -i ${DIR}/../plugins/pluginsreaderproviders/pcsc/atrparser.cpp ${DIR}/../plugins/pluginsreaderproviders/pcsc/atrparser.hpp
-
-find ${DIR}/../tests/unittest -name "*.cpp"  | xargs clang-format-3.8 -style=file -i {} \;
-clang-format-3.8 -style=file -i ${DIR}/../src/utils.cpp ${DIR}/../include/logicalaccess/utils.hpp
-
-clang-format-3.8 -style=file -i ${DIR}/../src/services/uidchanger/uidchangerservice.cpp ${DIR}/../include/logicalaccess/services/uidchanger/uidchangerservice.hpp
-
-clang-format-3.8 -style=file -i ${DIR}/../plugins/pluginscards/mifareultralight/mifareultralightuidchangerservice.{cpp,hpp}
-
-
-clang-format-3.8 -style=file -i ${DIR}/../include/logicalaccess/services/reader_service.hpp
-clang-format-3.8 -style=file -i ${DIR}/../include/logicalaccess/services/licensechecker/license_checker_service.hpp
-
-find ${DIR}/../include/logicalaccess/services/identity -name "*.hpp"  | xargs clang-format-3.8 -style=file -i {} \;
-find ${DIR}/../src/services/identity -name "*.cpp"  | xargs clang-format-3.8 -style=file -i {} \;
-
-find ${DIR}/../plugins/pluginscards/epass -name "*.hpp" -or -name "*.cpp"  | xargs clang-format-3.8 -style=file -i {} \;
-find ${DIR}/../plugins/pluginsreaderproviders/stidprg -name "*.cpp"  | xargs clang-format-3.8 -style=file -i {} \;
-find ${DIR}/../plugins/pluginsreaderproviders/stidprg -name "*.hpp"  | xargs clang-format-3.8 -style=file -i {} \;
-clang-format-3.8 -style=file -i ${DIR}/../src/cardprobe.cpp  ${DIR}/../include/cardprobe.cpp
-
-clang-format-3.8 -style=file -i ${DIR}/../plugins/pluginsreaderproviders/pcsc/readers/id3readerunit.hpp ${DIR}/../plugins/pluginsreaderproviders/pcsc/readers/id3readerunit.cpp
-
-clang-format-3.8 -style=file -i ${DIR}/../plugins/pluginsreaderproviders/pcsc/readers/cardprobes/*.hpp ${DIR}/../plugins/pluginsreaderproviders/pcsc/readers/cardprobes/*.cpp
+find ./include/logicalaccess ./src ./plugins ./tests ./samples ./vc++ \( -name '*.cpp' -or -name '*.hpp' \) | xargs clang-format-3.9 -style=file -i
