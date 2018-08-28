02c999ba880d414f0572ab15703126418ec62a94
Liryna <adrien.jund@islog.com>
2018-08-28 11:09:44 +0200

    Update git-buildpackage.sh

--- scripts/debian/git-buildpackage.sh
+++ scripts/debian/git-buildpackage.sh
@@ -8,8 +8,8 @@ if [ "$GIT_BRANCH" == "develop" ]; then
 
 	if [ "$CODENAME" == "stretch" ]; then
 		CODENAME="unstable"
-	elif [ "$CODENAME" == "zesty" ]; then
-		  CODENAME="zesty-backports"
+	elif [ "$CODENAME" == "bionic" ]; then
+		  CODENAME="bionic-backports"
 	fi  
   
 elif [ "$GIT_BRANCH" == "master" ]; then
