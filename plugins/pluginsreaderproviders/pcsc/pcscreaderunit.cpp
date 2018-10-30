646bc16d7f1ccda17e5ee796b0ebeda017340df4
Adrien JUND <adrien.jund@islog.com>
2018-10-30 15:34:19 +0100

    PCSC - Call proxy for beginTransaction / endTransaction

--- plugins/pluginsreaderproviders/pcsc/pcscreaderunit.cpp
+++ plugins/pluginsreaderproviders/pcsc/pcscreaderunit.cpp
@@ -1079,14 +1079,28 @@ namespace logicalaccess
 
     void PCSCReaderUnit::beginTransaction()
     {
-      if (SCARD_S_SUCCESS != SCardBeginTransaction(getHandle()))
-        THROW_EXCEPTION_WITH_LOG(CardException, "Failed SCardBeginTransaction");
+        if (d_proxyReaderUnit)
+        {
+            return d_proxyReaderUnit->beginTransaction();
+        }
+
+        LONG r = SCardBeginTransaction(getHandle());
+        if (SCARD_S_SUCCESS != r) {
+            THROW_EXCEPTION_WITH_LOG(CardException, "Failed SCardBeginTransaction error: " + std::to_string(r));
+        }
     }
 
     void PCSCReaderUnit::endTransaction(DWORD dwDisposition)
     {
-      if (SCARD_S_SUCCESS != SCardEndTransaction(getHandle(), dwDisposition))
-        THROW_EXCEPTION_WITH_LOG(CardException, "Failed SCardEndTransaction");
+        if (d_proxyReaderUnit)
+        {
+            return d_proxyReaderUnit->endTransaction(dwDisposition);
+        }
+
+        LONG r = SCardEndTransaction(getHandle(), dwDisposition);
+        if (SCARD_S_SUCCESS != r) {
+            THROW_EXCEPTION_WITH_LOG(CardException, "Failed SCardEndTransaction error: " + std::to_string(r));
+        }
     }
 
     unsigned long PCSCReaderUnit::getActiveProtocol() const
