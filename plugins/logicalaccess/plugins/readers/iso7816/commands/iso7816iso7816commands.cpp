d21d5cecfeafedfb83ba95b45dda6c8f4c1a66be
Maxhy <maxime.chamley@islog.com>
2018-12-17 11:57:29 +0100

    Fix iso7816 external authentication keyno parameter

--- plugins/logicalaccess/plugins/readers/iso7816/commands/iso7816iso7816commands.cpp
+++ plugins/logicalaccess/plugins/readers/iso7816/commands/iso7816iso7816commands.cpp
@@ -173,8 +173,7 @@ void ISO7816ISO7816Commands::externalAuthenticate(unsigned char algorithm,
                                                   unsigned char keyno,
                                                   const ByteVector &data)
 {
-    unsigned char p2 = 0x00;
-    keyno            = keyno & 0x0F;
+    unsigned char p2 = keyno & 0x0F;
     if (!globalReference)
     {
         p2 |= 0x80;
