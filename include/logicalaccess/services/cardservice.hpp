85022f605e0f1be4c59219179e9667fd10c1fab3
Kapp Arnaud <kapp.arno@gmail.com>
2019-05-03 17:10:44 +0200

    Replace typedef enum CardServiceType with enum CardServiceType.
    This have some issue with SWIG.

--- include/logicalaccess/services/cardservice.hpp
+++ include/logicalaccess/services/cardservice.hpp
@@ -31,7 +31,7 @@ class Chip;
 /**
  * \brief The card services.
  */
-typedef enum
+enum CardServiceType
 {
     CST_UNDEFINED      = 0x0000, /**< Undefined card service type */
     CST_ACCESS_CONTROL = 0x0001, /**< The access control card service type */
@@ -40,7 +40,7 @@ typedef enum
     CST_UID_CHANGER    = 0x0004, /**< Service to change the UID of a card */
     CST_IDENTITY       = 0x0005, /**< Service to query identity information (EPassport) */
     CST_JSON_DUMP      = 0x0006, /**< Service to dump a card content in JSON */
-} CardServiceType;
+};
 
 /**
  * \brief The base card service class for all services.
